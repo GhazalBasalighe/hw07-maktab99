4cfc94d5512d4cb644a6b8a94e2e62b8065d78da
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-08-19 19:57:24 +0330

    DOM working just fine. RAM and Hard disk filter still not available

--- Q7/Q7.js
+++ Q7/Q7.js
@@ -12,10 +12,10 @@ const products = [
   },
   {
     model: "Razer Blade 15",
-    processor: "Intel Core i7",
+    processor: "Intel Core i5",
     ram: "32 GB",
     hardDisk: "256GB SSD",
-    display: "16 inch",
+    display: "18 inch",
     graphics: "GTX 1660 Ti",
     os: "Windows 11",
     price: "$1,949.99",
@@ -26,7 +26,7 @@ const products = [
     processor: "Intel Core i7",
     ram: "16 GB",
     hardDisk: "1 TB SSD",
-    display: "16 inch",
+    display: "15 inch",
     graphics: "RTX 2070",
     os: "Windows 11",
     price: "$1,949.99",
@@ -37,38 +37,53 @@ const products = [
 // Get references to HTML elements
 const modelSelect = document.getElementById("model");
 const processorSelect = document.getElementById("processor");
-const ramFilter = document.getElementById("RAM-filter");
-const hardDiskFilter = document.getElementById("hard-disk-filter");
+// const ramFilter = document.getElementById("RAM-filter");
+// const hardDiskFilter = document.getElementById("hard-disk-filter");
 const displaySelect = document.getElementById("Display");
-const resultsTableBody = document.querySelector("#results-table-body");
+const resultsTableBody = document.getElementById("results-table-body");
 const searchButton = document.getElementById("search-button");
-const myForm = document.getElementById("salam");
+const form = document.getElementById("form");
 
-myForm.addEventListener("submit", function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
+  //prevent from refreshing
 });
 // Function to filter products based on selected options
 function filterProducts() {
   let results = products;
   const selectedModel = modelSelect.value;
-  console.log(selectedModel);
-  //   const selectedProcessor = processorSelect.value;
-  //   const selectedRam = ramFilter.value;
+  const selectedProcessor = processorSelect.value;
+  // const selectedRam = ramFilter.value;
   //   const selectedHardDisk = hardDiskFilter.value;
-  //   const selectedDisplay = displaySelect.value;
+  const selectedDisplay = displaySelect.value;
   if (selectedModel !== "placeholder") {
     results = results.filter((product) => {
       return product.model
         .toLowerCase()
         .includes(selectedModel.toLowerCase());
     });
-    updateResultsTable(results);
   }
+  if (selectedProcessor !== "placeholder") {
+    results = results.filter((product) => {
+      return product.processor
+        .toLowerCase()
+        .includes(selectedProcessor.toLowerCase());
+    });
+  }
+  if (selectedDisplay !== "placeholder") {
+    results = results.filter((product) => {
+      return product.display
+        .toLowerCase()
+        .includes(selectedDisplay.toLowerCase());
+    });
+  }
+  updateResultsTable(results);
 }
 
 // Function to update the table with filtered results
 function updateResultsTable(results) {
   resultsTableBody.innerHTML = "";
+  //clear the table each time
   for (const product of results) {
     const row = document.createElement("tr");
     row.innerHTML = `
@@ -90,4 +105,10 @@ function updateResultsTable(results) {
 // Attach event listener to the search button
 searchButton.addEventListener("click", function () {
   filterProducts();
+});
+//taking placeholders back
+document.addEventListener("DOMContentLoaded", function () {
+  modelSelect.value = "placeholder";
+  processorSelect.value = "placeholder";
+  displaySelect.value = "placeholder";
 });
